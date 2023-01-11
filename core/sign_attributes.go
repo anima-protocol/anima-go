6e097d856a9558660d37eebb6b01e012830bfdb6
Lucas ALLOIN <lucas@synaps.io>
2023-01-11 19:11:02 +0100

    :bug: fix sign attribute function

--- core/sign_attributes.go
+++ core/sign_attributes.go
@@ -19,7 +19,10 @@ func SignIssuing(anima *models.Protocol, issuer *protocol.AnimaIssuer, request *
 	if err != nil {
 		return nil, err
 	}
-	
+
+	if issuingAuthorization.PublicKeyEncryption != "" {
+		request.Document.EncryptionKey = issuingAuthorization.PublicKeyEncryption
+	}
 
 	// Sign Proof
 	proofContent, err := base64.StdEncoding.DecodeString(request.Proof.Content)
